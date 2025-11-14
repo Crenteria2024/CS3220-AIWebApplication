b58b491083e88f2855a79072570a1bf017bfa7e1
Edgard Zaragoza <edgardzaragoza719@gmail.com>
2025-11-13 20:24:03 -0800

    edgard

---
+++ src/main/java/com/example/cs3220aiwebapplication/Cs3220AiWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.cs3220aiwebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs3220AiWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs3220AiWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cs3220aiwebapplication/Cs3220AiWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cs3220aiwebapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs3220AiWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
